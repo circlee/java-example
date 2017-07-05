d99672b76037d7e8b50a8586502ba342c86d5f90
liweitang <liwt@tututech.net>
2017-07-05 18:17:10 +0800

    更新concurrent

---
+++ java-example-jvm/src/main/java/com/example/MemoryLeakTest2.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryLeakTest2 {
+
+    public static void main(String[] args) {
+        new Thread(new MemoryLeak(), "MemoryLeak").start();
+    }
+
+}
+
+class MemoryLeak implements Runnable {
+    public static List<Integer> leakList = new ArrayList<Integer>();
+
+    public void run() {
+        int count = 0;
+        while (true) {
+            try {
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+            }
+            count++;
+            Integer i = new Integer(count);
+            leakList.add(i);
+        }
+    }
+}
