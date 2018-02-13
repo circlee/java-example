44ed17e93d2892648ad0096f4bf09912c947b7dc
liweitang <liweitang@xingfeiinc.com>
2018-02-13 15:12:11 +0800

    添加rocketmq

---
+++ java-example-rocketmq/src/main/java/com/example/hello/HelloSender.java
@@ -0,0 +1,22 @@
+package com.example.hello;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class HelloSender {
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("test");
+        producer.start();
+
+        for (int i = 0; i < 100; i++) {
+            Message msg = new Message("hello", "a", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+
+}
