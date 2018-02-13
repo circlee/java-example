4bdb0543ff48e64a946fbf59f813e7196c28b741
liweitang <liweitang@xingfeiinc.com>
2018-02-13 18:25:52 +0800

    添加rocketmq例子

--- java-example-rocketmq/src/main/java/com/example/hello/HelloReceiver.java
+++ java-example-rocketmq/src/main/java/com/example/hello/HelloReceiver.java
@@ -2,9 +2,7 @@ package com.example.hello;
 
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 
@@ -15,28 +13,14 @@ public class HelloReceiver {
 
     public static void main(String[] argv) throws Exception {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test");
+        consumer.setNamesrvAddr("localhost:9876");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        consumer.subscribe("hello", "a");
-
-        consumer.registerMessageListener(new MessageListenerOrderly() {
-            AtomicLong consumeTimes = new AtomicLong(0);
-
+        consumer.subscribe("hello", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
-            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
-                context.setAutoCommit(false);
-                System.out.printf(Thread.currentThread().getName() + " Receive New Messages: " + msgs + "%n");
-                this.consumeTimes.incrementAndGet();
-                if ((this.consumeTimes.get() % 2) == 0) {
-                    return ConsumeOrderlyStatus.SUCCESS;
-                } else if ((this.consumeTimes.get() % 3) == 0) {
-                    return ConsumeOrderlyStatus.ROLLBACK;
-                } else if ((this.consumeTimes.get() % 4) == 0) {
-                    return ConsumeOrderlyStatus.COMMIT;
-                } else if ((this.consumeTimes.get() % 5) == 0) {
-                    context.setSuspendCurrentQueueTimeMillis(3000);
-                    return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
-                }
-                return ConsumeOrderlyStatus.SUCCESS;
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
         consumer.start();
--- java-example-rocketmq/src/main/java/com/example/hello/HelloSender.java
+++ java-example-rocketmq/src/main/java/com/example/hello/HelloSender.java
@@ -1,20 +1,30 @@
 package com.example.hello;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
 
 public class HelloSender {
 
     public static void main(String[] args) throws Exception {
         DefaultMQProducer producer = new DefaultMQProducer("test");
+        producer.setNamesrvAddr("localhost:9876");
         producer.start();
-
-        for (int i = 0; i < 100; i++) {
-            Message msg = new Message("hello", "a", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
-            SendResult sendResult = producer.send(msg);
-            System.out.printf("%s%n", sendResult);
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Message msg = new Message("hello", "hello-a", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult sendResult = producer.send(msg);
+                System.out.printf("%s%n", sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
         }
         producer.shutdown();
     }
